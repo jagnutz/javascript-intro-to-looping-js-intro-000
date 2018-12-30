25cef90dc8381538e2215f47127fdd53238f4094
Michael <chaphlux@yahoo.com>
2018-12-30 22:45:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -12,7 +12,7 @@ function makeArray() {
 */
 
 var array = [];
-function forLoop(array[]) {
+function forLoop(array) {
 for (let i = 0; i <= 25; i++){
   if (i<=1){
   array.push("I am " + ${i} + " strange loop.")
