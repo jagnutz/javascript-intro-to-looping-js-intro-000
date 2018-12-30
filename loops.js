cc242a8c38fbaff64842374d3f72f8c039c04bff
Michael <chaphlux@yahoo.com>
2018-12-30 23:15:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,11 +15,11 @@ var array = [];
 function forLoop(array) {
 for (let i = 0; i <= 25; i++;)
 {
-  if (i===1){
-  array.push("I am " + ${i} + " strange loop.")
+  if (i===0 || i===1){
+  array.push(`I am ${i} strange loop.`)
 }
 else {
-  array.push("I am " + ${i} + "strange loops.")
+  array.push(`I am ${i} strange loops.`)
 }
 
   return array
